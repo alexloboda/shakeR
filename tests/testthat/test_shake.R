aac03acfb78f4f3ce3c89fd2b5481af9d691b79c
Alexander Loboda <loboda@rain.ifmo.ru>
2017-11-08 18:47:25 -0500

    another simple test

--- tests/testthat/test_shake.R
+++ tests/testthat/test_shake.R
@@ -1,7 +1,13 @@
 context("shaker")
 
-test_that("pkg works on simple graph", {
+test_that("shaker doesn't crash on simple graph", {
   g <- make_ring(4)
   E(g)$class <- 1
   shake(g)
+})
+
+test_that("function cannot shuffle specific diamond", {
+  g <- make_ring(4)
+  E(g)$class <- c(1, 1, 2, 2)
+  expect_warning(shake(g))
 })
