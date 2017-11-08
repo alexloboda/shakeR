b3e6571b1cfc1ff5bd89ed79d4358b8369ff40ff
Alexander Loboda <loboda@rain.ifmo.ru>
2017-11-08 18:24:24 -0500

    couple of checks

--- R/shake.R
+++ R/shake.R
@@ -3,6 +3,9 @@
 #' @param graph accompanied by edge attribute `class` (1..n)
 shake <- function(graph, number_of_permutations = length(E(graph)) * 10,
                   hard_stop = number_of_permutations * 10) {
+  stopifnot("class" %in% list.edge.attributes(graph))
+  stopifnot(all(E(graph)$class > 0))
+
   df <- as_data_frame(graph, what = "edges")[c("from", "to", "class")]
   num_nodes <- length(V(graph))
   res <- shake_internal(df, num_nodes, number_of_permutations, hard_stop)
